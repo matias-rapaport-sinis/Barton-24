6c27b4a6f8a4993bfadee1ba7334d16a17dc41af
rapa-matias-sinis <matias.rapaport@sinis.com.ar>
2024-09-23 08:18:35 -0300

    Agrege el Design system para Barton basado en Bootstrap.

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Button variant="barton" >Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
-     </main>
-     </div>
+    <div>
+      <main>
+        
+      </main>
+    </div>
   );
 }
